5711bce9bd11931c09e2276092b6fbf3752916f7
Donghwan Kim <donghwan.kim92@gmail.com>
2022-05-23 17:03:48 +0200

    finishes devise setup

--- db/migrate/20220523142425_devise_create_users.rb
+++ db/migrate/20220523142425_devise_create_users.rb
@@ -13,7 +13,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
 
       ## Rememberable
       t.datetime :remember_created_at
-
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
