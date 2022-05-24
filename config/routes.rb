ce53c058c88286c79fdbd4cfd508aaaee4215644
Konstantin Greif <konstantin.greif@icloud.com>
2022-05-24 10:38:21 +0200

    Add bookings model, controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :capsules, only: [:index, :show, :new, :create]
+  resources :capsules, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:create, :update]
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
