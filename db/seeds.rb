2d5d5498345a63899e464d57e4ac37c29814987d
Konstantin Greif <konstantin.greif@icloud.com>
2022-05-24 12:33:08 +0200

    Add seeds for users and capsules

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  user = User.create({
+                       username: Faker::Name.unique.name,
+                       email: Faker::Internet.unique.email,
+                       password: '123456',
+                       password_confirmation: '123456'
+                     })
+  puts "Created user #{user.username}"
+end
+
+20.times do
+  capsule = Capsule.create({
+                             name: Faker::Vehicle.manufacture,
+                             description: Faker::Vehicle.model,
+                             price: rand(1.00..1000.00).round(2),
+                             user_id: rand(1..10),
+                             number: rand(1..2402)
+                           })
+  puts "Created capsule #{capsule.name}"
+end
