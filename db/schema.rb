af754a514fb5343b68554ce47c3c5450fb8f0c6d
jeremy bordat <j.bordat@live.fr>
2022-05-24 17:31:46 +0200

    cleaning_banner_picture:out_logo:refresh

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_24_103812) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_24_124705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_103812) do
     t.datetime "updated_at", null: false
     t.integer "number"
     t.index ["user_id"], name: "index_capsules_on_user_id"
+  end
+
+  create_table "home_pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
