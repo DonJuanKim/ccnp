b127b69a223afc017017a86ed086a9a96dbb7977
Johannes Ibald <j.ibald@protonmail.ch>
2022-05-24 12:07:04 +0200

    creates booking policy files

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,12 +1,8 @@
-class CapsulePolicy < ApplicationPolicy
+class BookingPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
     #   scope.all
     # end
-  end
-
-  def show
-    true
   end
 end
