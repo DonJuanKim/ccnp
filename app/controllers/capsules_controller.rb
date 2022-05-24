a8e2c242f0785135f65ff8959413b44ccab7f8b7
Johannes Ibald <j.ibald@protonmail.ch>
2022-05-24 12:52:17 +0200

    adds cloudinary gem, multiple photos to capsule model, templates for capsule_params method in controller

--- app/controllers/capsules_controller.rb
+++ app/controllers/capsules_controller.rb
@@ -14,4 +14,18 @@ class CapsulesController < ApplicationController
   def create
     raise
   end
+
+  # TODO: implement capsule_params, do not forget :photo
+  # should look something like this:
+  # CAREFUL: possibly incomplete .permit parameter list
+  #
+  # for one photo
+  # def capsule_params
+  #   params.require(:capsule).permit(:name, :description, :price, :number, :photo)
+  # end
+  #
+  # for multiple photos
+  # def capsule_params
+  #   params.require(:capsule).permit(:name, :description, :price, :number, photos: [])
+  # end
 end
