601a4b652b6587667a65d9b55c89c4161e7d039b
Konstantin Greif <konstantin.greif@icloud.com>
2022-05-25 13:17:05 +0200

    Add enum for status and pictures to show

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,10 @@ class Booking < ApplicationRecord
   belongs_to :capsule
 
   validates :period_start, :period_end, :status, :user, :capsule, presence: true
+
+  enum status: {
+    pending: 1,
+    accepted: 2,
+    declined: 3
+  }
 end
