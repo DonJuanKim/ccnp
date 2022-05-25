db489bb172bf7bad2dd8b36ee1040bd5f166e630
Donghwan Kim <donghwan.kim92@gmail.com>
2022-05-25 15:10:23 +0200

    users' capsules show booking IDs

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,7 +3,4 @@ class DashboardsController < ApplicationController
     @capsules = policy_scope(Capsule.where(user_id: current_user.id))
     @created_bookings = policy_scope(Booking.where(user_id: current_user.id))
   end
-
-  
-
 end
