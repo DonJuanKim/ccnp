3e47c845ec92e96caaf9af0fe3172e9a6531eedd
Johannes Ibald <j.ibald@protonmail.ch>
2022-05-26 15:12:11 +0200

    removes comments, adds comments

--- app/javascript/controllers/edit_form_controller.js
+++ app/javascript/controllers/edit_form_controller.js
@@ -5,23 +5,11 @@ export default class extends Controller {
   static targets = [ "editForm" ]
 
   connect() {
-    console.log("edit_form_controller testing");
+    // console.log("edit_form_controller testing");
   }
 
   toggleEdit() {
-    console.log("toggleEdit testing");
+    // console.log("toggleEdit testing");
     this.editFormTarget.classList.toggle("d-none")
   }
 }
-
-
-
-// const element = document.getElementById("edit-btn");
-// console.log(element.innerText);
-// element.addEventListener("click", (event) => {
-//   // Do something (callback)
-//   element.innerText = "Bingo!";
-//   const audio = new Audio('sound.mp3');
-//   audio.play();
-//   element.classList.add("disabled");
-// });
